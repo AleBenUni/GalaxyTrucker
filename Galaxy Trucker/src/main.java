92ef859ea751b6137d6237c9abdc8e884996a02a
aliAmme <a.ammerti@studenti.unibg.it>
2025-04-19 20:29:01 +0200

    Ho fatto abbastanza esperimenti, ho capito come nascondere l'utente fantasma, come mai non bisogna fare una Push se non si ha fatto prima una Pull e come far funzionare il collegamento tra Eclipse e GitHub

--- Galaxy Trucker/src/main.java
+++ Galaxy Trucker/src/main.java
@@ -1,14 +1,8 @@
-
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//edit main
-<<<<<<< HEAD
-		//che succede se non eseguo una Pull
-=======
-		//oggi e' il 19 Aprile 2025
->>>>>>> branch 'main' of https://github.com/AleBenUni/GalaxyTrucker
+		
+		//Sono un main senza un codice, che stai aspettando?!
 
 	}
 
