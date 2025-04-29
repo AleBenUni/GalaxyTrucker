1f5adb6443b1d11f2ddb7f1ca0a3478e00a57a07
benig <benig@AsusG16>
2025-04-29 14:35:45 +0200

    Costruttore componenti

--- Galaxy Trucker/src/componenti/Componenti.java
+++ Galaxy Trucker/src/componenti/Componenti.java
@@ -2,5 +2,30 @@ package componenti;
 
 public class Componenti {
 
-	//Test3
+	private Connettori up;
+	private Connettori dx;
+	private Connettori sx;
+	private Connettori dw;
+	
+	public Componenti(Connettori up, Connettori dx, Connettori sx, Connettori dw) {
+		this.up=up;
+		this.dx=dx;
+		this.sx=sx;
+		this.dw=dw;
+	}
+	
+	public Connettori getConnettori(String pos) {
+		switch(pos){
+		case "up":
+			return up;
+		case "dx":
+			return dx;
+		case "sx":
+			return sx;
+		case "dw":
+			return dw;
+			
+		}
+		return null;
+	}
 }
