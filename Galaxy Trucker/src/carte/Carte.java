b23db893d1c1b312aa24057711c03819b6483da8
aliAmme <a.ammerti@studenti.unibg.it>
2025-04-30 15:33:09 +0200

    Start creating cards

---
+++ Galaxy Trucker/src/carte/Carte.java
@@ -0,0 +1,11 @@
+package carte;
+
+public class Carte {
+
+	
+	/*in comune le carte hanno: 
+	 * Livello (I, II, III)
+	 * Giorni di volo persi/guadagnato [Indicato con un triangolo rivolto verso il basso/verso l'alto]
+	 *  (tranne: pioggia di metiorite [Zero giorni persi], 
+	 */
+}
---
+++ Galaxy Trucker/src/carte/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Package containing the Galaxy Trucker game cards
+ * <p>
+ * This package contains the Java code 
+ * for the 150 cards of the "well-known" board game <b>Galaxy Trucker</b>
+ * </p>
+ *
+ * @since 1.0
+ * @author https://github.com/AleBenUni/GalaxyTrucker
+ * @version 1.0
+ */
+package carte;
