147879b28e00fbeffc32ea6bc91e402ce303bfd9
benig <benig@AsusG16>
2025-04-30 11:34:50 +0200

    Add class Batteria with methods

---
+++ Galaxy Trucker/src/componenti/Batteria.java
@@ -0,0 +1,44 @@
+package componenti;
+
+public class Batteria extends Componente{
+	int nEnergie;
+	int grandezzaBatteria;
+	public Batteria(int grandezzaBatteria, Connettore up, Connettore dx, Connettore sx, Connettore dw) {
+		super(up,dx,sx,dw);
+		this.grandezzaBatteria=grandezzaBatteria;
+		this.nEnergie=grandezzaBatteria;
+	}
+	
+	public int getGrandezzaBatteria() {
+		return grandezzaBatteria;
+	}
+	
+	public int getNEnergie() {
+		return nEnergie;
+	}
+	
+	public boolean setNEnergie(int nEnergie) {
+		if(nEnergie<=grandezzaBatteria&&nEnergie>=0) {
+			this.nEnergie=nEnergie;
+			return true;
+		}
+		return false;	
+	}
+	
+	public boolean addEnergia(int nEnergie) {
+		if((this.nEnergie+nEnergie)<=grandezzaBatteria&&nEnergie>=0) {
+			this.nEnergie+=nEnergie;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean minusEnergia(int nEnergie) {
+		if((this.nEnergie-nEnergie)>0&&nEnergie>=0) {
+			this.nEnergie-=nEnergie;
+			return true;
+		}
+		return false;
+	}
+	
+}
