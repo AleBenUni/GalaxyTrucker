e04b4158ca09ad860af1470126e83db3bdf08a12
aliAmme (UNIBG) <a.ammerti@studenti.unibg.it>
2025-05-01 18:45:27 +0200

    Having established the design of the cards, a doubt to think about later

--- Galaxy Trucker/src/carte/Carte.java
+++ Galaxy Trucker/src/carte/Carte.java
@@ -3,9 +3,62 @@ package carte;
 public class Carte {
 
 	
-	/*in comune le carte hanno: 
-	 * Livello (I, II, III)
-	 * Giorni di volo persi/guadagnato [Indicato con un triangolo rivolto verso il basso/verso l'alto]
-	 *  (tranne: pioggia di metiorite [Zero giorni persi], 
+	/*
+	 * IN COMUNE
+	 * ---------------------------------------
+	 * 1. Livello: 
+	 *    - Classificazione in I, II, III
+	 * 
+	 * 2. Giorni di volo: 
+	 *    - Modificatore (+/- giorni) 
+	 *    - Eccezioni (0 giorni):
+	 *      a) Pioggia di Meteoriti 
+	 *      b) Epidemia 
+	 *      c) Sabotaggio
+	 * 
+	 * 3. Merci: 
+	 *    - Guadagno/Perdita di 4 tipi (priorità nella perdita: VIOLA > ORO > VERDE > BLUE) 
+	 *    		(possibile uso di enum)
+	 *    - Eccezioni (0 merci):
+	 *      a) Nave Abbandonata
+	 *      b) Schiavisti/Pirati (ma la sua carta gemella viola "contrabbandieri" tratta le merci, 
+	 *      	quindi che fare, unirle in un gruppo solo, tenerle separate o creare 
+	 *      	un ragruppamento per colore?)
+	 *      c) Carte gialle
+	 *      d) Spazio Aperto
+	 * 
+	 * 4. Equipaggio (umani e alieni): 
+	 *    - Solo perdita
+	 *    - Eccezioni (0 perdite):
+	 *      a) Pioggia di Meteoriti
+	 *      b) Pianeti
+	 *      c) Nave Abbandonata
+	 *      d) Pirati e Contrabbandieri (sono carte viole, Schiavisti perde equipaggio)
+	 *      e) Sabotaggio e Polvere stellare  (sono carte gialle, Epidemia perde equipaggio)
+	 *      f) Stazione Abbandonata
+	 * 
+	 * 5. Crediti: 
+	 *    - Solo guadagno (mai perdita)
+	 *    - Presente solo in:
+	 *      a) Nave Abbandonata
+	 *      b) Schiavisti e Pirati (In "Contrabbandieri" si guadagna merce al posto dei crediti)
+	 * 
+	 * 
+	 * NON IN COMUNE
+	 * -----------------------
+	 * 1. Pioggia di Meteoriti: 
+	 *    - Simile a Zona di Guerra ma con meccaniche diverse, la considero non comune
+	 * 
+	 * 2. Sabotaggio: 
+	 *    - Distrugge un componente (simile a meteorite e cannonate), ma è un colpo sicuro,
+	 *   determinato con tre lanci di dadi. Presente solo una volta tra le 150 carte,
+	 *   la considero non comune.
+	 *   
+	 * 
+	 * NOTE/DUBBI APERTI
+	 * -----------------
+	 *	  - Cannonate:
+	 *	  Presenti in Zona di guerra e Pirati. Hanno una logica dietro l’effetto.
+ 	 *	  Da valutare se considerare cannonate come comune a tutte le carte o meno.
 	 */
 }
