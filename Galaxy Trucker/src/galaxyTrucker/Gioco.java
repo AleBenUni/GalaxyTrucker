38980cab74ccb8a64800ca2ebc4fb841f2cbe5db
AleBenUni <a.benigni3@studenti.unibg.it>
2025-05-06 15:50:25 +0200

    Bug Fix

--- Galaxy Trucker/src/galaxyTrucker/Gioco.java
+++ Galaxy Trucker/src/galaxyTrucker/Gioco.java
@@ -4,11 +4,17 @@ import carte.Livello;
 public class Gioco {
 	
 	private Nave[] nave;
+	private Livello livello;
 	
 	public Gioco(int nGiocatori, Livello livello) {
+		this.livello=livello;
 		nave=new Nave[nGiocatori];
 		for(int i=0;i<nGiocatori;i++)
 			nave[i]=new Nave(livello);
+	}
+	
+	public Livello getLivello() {
+		return livello;
 	}
 	
 }
--- Galaxy Trucker/src/galaxyTrucker/Nave.java
+++ Galaxy Trucker/src/galaxyTrucker/Nave.java
@@ -9,6 +9,7 @@ public class Nave {
 	public Nave(int sizeX, int sizeY) {
 		this.sizeX=sizeX;
 		this.sizeY=sizeY;
+		this.cella=new Cella[sizeX][sizeY];
 		for(int i=0;i<sizeX;i++)
 			for(int j=0;j<sizeY;j++)
 				cella[i][j]=new Cella(null, new Posizione(i,j));
