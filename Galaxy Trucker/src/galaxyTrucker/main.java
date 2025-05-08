60db9786c0e7b38529b735e41b1c93c00a04427b
aliAmme <a.ammerti@studenti.unibg.it>
2025-05-08 13:05:04 +0200

    Eliminato spazio main

--- Galaxy Trucker/src/galaxyTrucker/main.java
+++ Galaxy Trucker/src/galaxyTrucker/main.java
@@ -6,10 +6,8 @@ public class main {
 
 	public static void main(String[] args) {
         
-        Carte Uno = new Carte(Livello.I, 5, 0, 100);
-        
-        System.out.println("Oggetto Carte creato con successo.");
-        
+        Carte Uno = new Carte(Livello.I, 5, 0, 100);    
+        System.out.println("Oggetto Carte creato con successo.");      
         System.out.println(Uno);       
 }
 
