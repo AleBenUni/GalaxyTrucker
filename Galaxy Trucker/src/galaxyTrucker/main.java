69b1e3e3e5807fdcfd6c4942bc0d9953e2c63029
aliAmme <a.ammerti@studenti.unibg.it>
2025-05-08 12:08:40 +0200

    Ennesimo test

--- Galaxy Trucker/src/galaxyTrucker/main.java
+++ Galaxy Trucker/src/galaxyTrucker/main.java
@@ -4,8 +4,13 @@ import carte.Livello;
 
 public class main {
 
-	//main di prova, creato per verificare la funzionalita di carta. Modificalo senza problemi
-	
-	hgfhf
+	public static void main(String[] args) {
+        
+        Carte Uno = new Carte(Livello.I, 5, 0, 100);
+        
+        System.out.println("Oggetto Carte creato con successo.");
+        
+        System.out.println(Uno);
+}
 
 }
