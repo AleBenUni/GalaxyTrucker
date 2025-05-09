7610002ebbca0c818a54bba5f04a7a5812f73066
aliAmme <a.ammerti@studenti.unibg.it>
2025-05-09 08:53:22 +0200

    Rethought and partly restructured cards

--- Galaxy Trucker/src/carte/Carte.java
+++ Galaxy Trucker/src/carte/Carte.java
@@ -8,23 +8,24 @@ public class Carte {
 	
 	private final int giorniVolo; 
 	private final int credito; 
-	private final int perditeEquipaggio;
+	private final int equipaggio;
 	
 
 	private Livello livello;
 
-//richiama in tabellone un metodo
-	public Carte(Livello livello, int giorniVolo, int perditeEquipaggio, int credito) {
+//richiama da tabellone la funzione riguardante la perdita di equipaggio
+	public Carte(Livello livello, int giorniVolo, int equipaggio, int credito) {
 	    this.giorniVolo = giorniVolo;
-	    this.perditeEquipaggio = perditeEquipaggio;
+	    this.equipaggio = equipaggio;
 	    this.livello= livello;
 	    this.credito= credito;
 	}
 
+	
 
 	@Override
 	public String toString() {
-		return "Carte [giorniVolo=" + giorniVolo + ", credito=" + credito + ", perditeEquipaggio=" + perditeEquipaggio
+		return "Carte [giorniVolo=" + giorniVolo + ", credito=" + credito + ", perditeEquipaggio=" + equipaggio
 				+ ", livello=" + livello + "]";
 	}
 	
