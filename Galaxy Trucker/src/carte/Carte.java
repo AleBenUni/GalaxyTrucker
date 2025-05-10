66af441d895557f31a24b68883331879907c2aca
aliAmme <a.ammerti@studenti.unibg.it>
2025-05-10 20:33:58 +0200

    Work begins on the 150 card file

--- Galaxy Trucker/src/carte/Carte.java
+++ Galaxy Trucker/src/carte/Carte.java
@@ -9,24 +9,46 @@ public class Carte {
 	private final int giorniVolo; 
 	private final int credito; 
 	private final int equipaggio;
+	private final int merce;
 	
 
 	private Livello livello;
 
 //richiama da tabellone la funzione riguardante la perdita di equipaggio
-	public Carte(Livello livello, int giorniVolo, int equipaggio, int credito) {
+	public Carte(Livello livello, int giorniVolo, int merce, int equipaggio, int credito) {
 	    this.giorniVolo = giorniVolo;
 	    this.equipaggio = equipaggio;
 	    this.livello= livello;
 	    this.credito= credito;
+	    this.merce= merce;
 	}
 
-	
+	public Livello getLivello() {
+		return livello;
+	}
+
+	public int getGiorniVolo() {
+		return giorniVolo;
+	}
+
+	public int getCredito() {
+		return credito;
+	}
+
+	public int getEquipaggio() {
+		return equipaggio;
+	}
+
+	public int getMerce() {
+		return merce;
+	}
 
 	@Override
 	public String toString() {
-		return "Carte [giorniVolo=" + giorniVolo + ", credito=" + credito + ", perditeEquipaggio=" + equipaggio
-				+ ", livello=" + livello + "]";
+		return "Carte [ Livello carta=" + livello + ", Giorni di volo=" + giorniVolo + ", Credito guadagnato=" + credito + ", Equipaggio perso=" + equipaggio + ", Merce="
+				+ merce + "]\nIl + indica un guadagno, il - indica una perdita (solo per merce e Giorni di volo).";
 	}
+	
+	
 	
 }
--- Galaxy Trucker/src/galaxyTrucker/main.java
+++ Galaxy Trucker/src/galaxyTrucker/main.java
@@ -6,7 +6,7 @@ public class main {
 
 	public static void main(String[] args) {
         
-        Carte Uno = new Carte(Livello.I, 5, 0, 100);    
+        Carte Uno = new Carte(Livello.I, -2, 12, 3, 10);    
         System.out.println("Oggetto Carte creato con successo.");      
         System.out.println(Uno);
         
