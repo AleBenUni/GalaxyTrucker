286c523ecda8c75d2111389a15ef905c65f679d3
Ouardia12 <o.agountre@studenti.unibg.it>
2025-05-25 21:44:53 +0200

    update the code Modstru

--- Galaxy Trucker/src/componenti/Modstru.java
+++ Galaxy Trucker/src/componenti/Modstru.java
@@ -2,13 +2,13 @@ package componenti;
 
 public class Modstru {
 	
-	 private String nome;
+	
 	    private Connettore[] lati = new Connettore[4]; // Indici: 0 = Nord, 1 = Est, 2 = Sud, 3 = Ovest
 
 	    // Costruttore: inizializza nome e i 4 connettori
 	    public Modstru(String nome, Connettore nord, Connettore est,
 	                   Connettore sud, Connettore ovest) {
-	        this.nome = nome;
+	    	
 	        this.lati[0] = nord;
 	        this.lati[1] = est;
 	        this.lati[2] = sud;
@@ -20,10 +20,7 @@ public class Modstru {
 	        return lati[mioLato].puoConnettersi(altro.lati[latoAltro]);
 	    }
 
-	    // Restituisce il nome del modulo
-	    public String getNome() {
-	        return nome;
-	    }
+	   
 
 	    // Restituisce il connettore su un lato specifico
 	    public Connettore getConnettore(int lato) {
