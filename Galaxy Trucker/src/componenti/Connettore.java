f2025fcc1a26fdd750a7be04bf4b0045f9e69097
AleBenUni <a.benigni3@studenti.unibg.it>
2025-05-27 00:40:09 +0200

    Fix enum

--- Galaxy Trucker/src/componenti/Connettore.java
+++ Galaxy Trucker/src/componenti/Connettore.java
@@ -2,20 +2,4 @@ package componenti;
 
 public enum Connettore {
 	assente,singolo, doppio, multiplo;
-	
-	    // Metodo che verifica se due connettori possono collegarsi tra loro
-	    public boolean puoConnettersi(Connettore altro) {
-	        // Se uno dei due è MULTIPLO, la connessione è sempre possibile
-	        if (this == MULTIPLO || altro == MULTIPLO) {
-	            return true;
-	        }
-
-	        // Se uno dei due è ASSENTE, non si possono collegare
-	        if (this == ASSENTE || altro == ASSENTE) {
-	            return false;
-	        }
-
-	        // Altrimenti si possono collegare solo se sono dello stesso tipo
-	        return this == altro;
-	    }
 }
