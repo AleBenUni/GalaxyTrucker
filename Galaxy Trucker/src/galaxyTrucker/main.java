6618d35899bf5ff93e3d8ef29a81707ba9aa0087
AleBenUni <a.benigni3@studenti.unibg.it>
2025-05-28 19:57:41 +0200

    Modifiche per test

--- Galaxy Trucker/src/galaxyTrucker/main.java
+++ Galaxy Trucker/src/galaxyTrucker/main.java
@@ -1,20 +1,14 @@
 package galaxyTrucker;
 import carte.Carta;
+import carte.Livello;
 import carte.Mazzo;
 
 public class main {
 
 	public static void main(String[] args) {
         
-		//questo main è solo una prova del file csv. Può essere modificato senza preavviso
-		
-		Mazzo mazzo = new Mazzo(false);
-        
-       Carta pescata = mazzo.pescadalMazzo();
-       // System.out.printf("\nHa pescato--> " + pescata);
-        mazzo.rivelaCarteMazzo();
-        mazzo.svuotaMazzo();
-        mazzo.rivelaCarteMazzo();
+		Nave nave=new Nave(Livello.II);
+		nave.visualizzaNave();
 }
 
 }
