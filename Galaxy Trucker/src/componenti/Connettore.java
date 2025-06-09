89a9d84393a45ffcc46107ffe366b9963549e283
AleBenUni <a.benigni3@studenti.unibg.it>
2025-06-09 18:23:36 +0200

    Add metodo

--- Galaxy Trucker/src/componenti/Connettore.java
+++ Galaxy Trucker/src/componenti/Connettore.java
@@ -2,4 +2,15 @@ package componenti;
 
 public enum Connettore {
 	assente,singolo, doppio, multiplo;
+	
+	public boolean connection(Connettore connettore) {
+		if(this==multiplo&&connettore!=assente || this!=assente&&connettore==multiplo)
+			return true;
+		else if(this==assente&&connettore==assente)
+			return true;
+		else if(this==connettore)
+			return true;
+		else
+			return false;
+	}
 }
