be82ac4ec2583abb2fe9454025a19c5fa0eb73d1
aliAmme <a.ammerti@studenti.unibg.it>
2025-06-10 21:34:36 +0200

    Created getFlottaNaveOrdinata() to get ships sorted from highest to lowest in days

--- Galaxy Trucker/src/galaxyTrucker/Gioco.java
+++ Galaxy Trucker/src/galaxyTrucker/Gioco.java
@@ -2,6 +2,10 @@ package galaxyTrucker;
 import carte.Livello;
 import componenti.Mucchio;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
@@ -126,6 +130,23 @@ public class Gioco {
 			return this.nave[nave];
 		else
 			return null;
+	}
+	
+	public List<Nave> getFlottaNaveOrdinata(){
+		Integer[] giorniNavi = new Integer [this.getNGiocatori()];
+    	List <Nave> giocatori = new ArrayList<>();
+    	for (int i=0; i<this.getNGiocatori(); i++) {
+    		giorniNavi[i]=this.getNave(i).getGiorniVolo();
+    	}
+    	Arrays.sort(giorniNavi, Collections.reverseOrder());
+    	for (int i=0; i<this.getNGiocatori(); i++) {
+    		for (int j=0; j<this.getNGiocatori(); j++) {
+    			if (giorniNavi[j]==this.getNave(i).getGiorniVolo()) {
+    				giocatori.add(this.getNave(i));
+    			}
+    		}
+    	}
+    	return giocatori;
 	}
 	
 	public Plancia getPlancia() {
